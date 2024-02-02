50c77f23b79ced961dde62808409685a392193b8
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-02-02 23:08:07 +0530

    Implementation

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/checkinRoute.js');
+const userrouter = require("./routes/authorization.js")
 const app = express();
-const PORT = 8001;
+const PORT = 5001;
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/UniSafedb',);
@@ -11,7 +12,7 @@ app.use(express.json());
 
 // Endpoint for checking in
 app.use('/checkin', router);  // Use router.checkinRoute directly
-
+app.use('/',userrouter);
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
