bbd38ec27221f06e7657a12ada2dd6ff496bbc5e
KingrogKDR <abhisheksaikia766@gmail.com>
2024-02-02 23:43:42 +0530

    home page initialized

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,23 +4,17 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
-            <nav className="bg-gray-100 border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-gray-100 border-gray-200 px-4 lg:px-1 py-2">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <span className='py-2 px-1 ml-1 text-lg font-semibold text-blue-500'>Uni<span className='font-bold text-purple-500'>Safe</span></span>
                     </Link>
-                    <div className="flex items-center lg:order-2">
+                    <div className="items-end lg:order-2">
                         <Link
                             to="/login"
-                            className="text-gray-800 hover:bg-sky-50 hover:ring-2 hover:ring-sky-200 focus:ring-2 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-gradient-to-t from-violet-600 to-purple-400 hover:bg-sky-50 hover:ring-2 hover:ring-sky-200 focus:ring-2 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 focus:outline-none transition duration-200"
                         >
                             Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-purple-600 hover:bg-orange-800 focus:ring-2 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
                         </Link>
                     </div>
                     <div
