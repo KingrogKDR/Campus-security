0e2abc5f77c449a19ff55db9cea69dd8a7fe9d14
Abhilash <abhilashkashyap33@gmail.com>
2024-02-02 14:30:16 +0530

    Added models - Abhilash

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  // Other user fields...
-});
+  name: {
+    type: String,
+    required: true,
+  },
+  aadhar_number: {
+    type: Number,
+    min: 12,
+    max: 12,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  dob: {
+    type: Date,
+    required: true,
+  }
 
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
