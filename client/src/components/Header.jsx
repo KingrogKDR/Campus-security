d16c23fa0d8ba19e6c4c1f37b08390e113e090bb
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-02-03 10:24:34 +0530

    testing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -37,7 +37,7 @@ console.log(userInfo);
                         <Link
                             to="/login"
                             className="text-white bg-gradient-to-t from-violet-600 to-purple-400 hover:bg-sky-50 hover:ring-2 hover:ring-sky-200 focus:ring-2 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 focus:outline-none transition duration-200"
-                        >
+                        > 
                             Log in
                         </Link>
                         <Link
