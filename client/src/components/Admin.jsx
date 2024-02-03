e8e86e5fcd3c09565049a586786a739efdca0035
KingrogKDR <abhisheksaikia766@gmail.com>
2024-02-03 20:15:56 +0530

    Home updated

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -8,10 +8,10 @@ const Admin = () => {
         <div className='lg:ml-[290px] lg:space-y-10 space-y-3'>
           <p className='p-8 font-semibold text-2xl lg:text-4xl shadow-lg'>Dashboard</p>
           <div className='flex flex-wrap items-center justify-center mt-10'>
-            <div className='bg-white pt-10 pb-16 px-8 w-11/12 mt-2 text-2xl'>Students</div>
+            <div className='bg-white pt-10 pb-16 px-8 w-11/12 mt-2 text-2xl'>Visitors</div>
           </div>
           <div className='flex flex-wrap items-center justify-center'>
-            <div className='bg-white pt-10 pb-16 px-8 w-11/12 mt-2 text-2xl'>Students</div>
+            <div className='bg-white pt-10 pb-16 px-8 w-11/12 mt-2 text-2xl'>Faculties</div>
           </div>
           <div className='flex flex-wrap items-center justify-center'>
             <div className='bg-white pt-10 pb-16 px-8 w-11/12 mt-2 text-2xl'>Students</div>
