aff5c92461ac8874b350467be8dfb53ac79e24f2
KingrogKDR <abhisheksaikia766@gmail.com>
2024-02-03 10:16:51 +0530

    Landing page added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,39 @@
 import React from "react";
-
 export default function Home(){
     return(
         <>
             <div className="w-full h-screen inset-0">
-                <div className="w-full h-1/3">
-                    <img src="" alt="Nit-Silchar" />
+                <div className="w-full h-2/5 lg:h-4/5 bg-no-repeat bg-cover mb-5" style={{backgroundImage:`url("download.webp")`}}>
+                    <div className="w-full h-full text-center backdrop-blur-[6px] pt-20 lg:pt-52">
+                        <h1 className=" text-3xl lg:text-[72px] font-extrabold">Want to Visit NIT Silchar</h1> 
+                        <p className="mt-12 text-lg lg:text-3xl font-semibold lg:mt-24">Get your personalized id card for your visits</p>
+                        <p className="mt-14 lg:mt-40 text-xs lg:text-xl">Scroll to learn more</p>
+                    </div>
+                </div>
+                <div className="w-full h-3/5 bg-purple-200">
+                    <p className="text-center py-10 font-bold text-3xl lg:text-[48px] text-slate-700">Steps to follow</p>
+                    <div className="flex justify-center items-center gap-6 lg:gap-56 mt-6">
+                        <div className="space-y-3 text-sm lg:text-xl">
+                            <p><span className="mr-2 bg-white rounded-full px-2">1</span>Register at the Gate</p>
+                            <p>of the University</p>
+                            <div className="space-y-3 pt-12">
+                                <p><span className="mr-2 bg-white rounded-full px-2">2</span>Get your personalised</p>
+                                <p>temporary id card</p>
+                            </div>
+                        </div>
+                        <div className="">
+                            <img alt="qr-code" />
+                        </div>
+                        <div className="space-y-3 text-sm lg:text-xl">
+                            <p className="pt-6"><span className="mr-2 bg-white rounded-full px-2">3</span>Scan your id at the</p>
+                            <p>places you want</p>
+                            <p> to visit</p>
+                            <div className="space-y-3 pt-10">
+                                <p><span className="mr-2 bg-white rounded-full px-2">4</span>Enjoy your visit</p>
+                                <p>at NIT Silchar</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,24 +4,20 @@ import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout'
 import Login from './components/Login'
-<<<<<<< HEAD
 import { UserContextProvider } from './providers/UserProvider'
 import Dashboard from './components/Dashboard'
 import Register from './components/Register'
 import Admin from './components/Admin'
-=======
-import Register from './components/Register'
->>>>>>> 525d15a6bca658b5c05673b122798ab1bf873a99
+import Home from './components/Home'
+
 
 const myRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
+    <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login />}/>
-<<<<<<< HEAD
       <Route path='/dashboard' element={<Dashboard />}/>
       <Route path='/admin' element={<Admin />}/>
-=======
->>>>>>> 525d15a6bca658b5c05673b122798ab1bf873a99
       <Route path='/register' element={<Register />}/>
     </Route>
   )
@@ -29,6 +25,9 @@ const myRouter = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UserContextProvider>
+
     <RouterProvider router={myRouter}/>
+    </UserContextProvider>
   </React.StrictMode>
 )
