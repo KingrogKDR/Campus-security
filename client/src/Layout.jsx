28e43bb8b1cb1fd55302e54b3127cbc8fa516cc9
Abhishek Saikia <149085408+KingrogKDR@users.noreply.github.com>
2024-02-03 12:10:23 +0530

    Update Layout.jsx

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet ,useLocation} from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Header1 from "./components/hEADER1.JSX";
+import Header1 from "./components/Header1.JSX";
 
 function Layout(){
     const location = useLocation()
